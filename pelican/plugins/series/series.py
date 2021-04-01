0ded246295e5ab1b3c2ca4743e7cd3fea8954e96
Johan Vergeer <johanvergeer@gmail.com>
2021-04-01 23:24:05 +0200

    Refactor series module and add type hints

--- pelican/plugins/series/series.py
+++ pelican/plugins/series/series.py
@@ -8,59 +8,72 @@ manage the series in the template.
 """
 
 from collections import defaultdict
-from operator import itemgetter
+from dataclasses import dataclass
+from datetime import date
+from operator import attrgetter
+from typing import Dict, List, Optional
+
+from pelican.contents import Article
+from pelican.generators import ArticlesGenerator
 
 from pelican import signals
 
 
-def aggregate_series(generator):
-    series = defaultdict(list)
+@dataclass
+class SeriesArticle:
+    index: Optional[int]
+    date: date
+    article: Article
+
+
+def _sort_articles(series_articles: List[SeriesArticle]) -> List[Article]:
+    # This is not DRY but very simple to understand
+    forced_order_articles = [
+        article for article in series_articles if article.index is not None
+    ]
+
+    date_order_articles = [
+        article for article in series_articles if article.date is None
+    ]
+
+    forced_order_articles.sort(key=attrgetter("index"))
+    date_order_articles.sort(key=attrgetter("date"))
+
+    all_articles = forced_order_articles + date_order_articles
+    return [article.article for article in all_articles]
+
+
+def aggregate_series(generator: ArticlesGenerator) -> None:
+    series: Dict[str, List[SeriesArticle]] = defaultdict(list)
 
     # This cycles through all articles in the given generator
     # and collects the 'series' metadata, if present.
     # The 'series_index' metadata is also stored, if specified
     for article in generator.articles:
         if "series" in article.metadata:
-            article_entry = (
-                article.metadata.get("series_index", None),
-                article.metadata["date"],
-                article,
+            series[article.metadata["series"]].append(
+                SeriesArticle(
+                    article.metadata.get("series_index", None),
+                    article.metadata["date"],
+                    article,
+                )
             )
-
-            series[article.metadata["series"]].append(article_entry)
 
     # This uses items() which on Python2 is not a generator
     # but we are dealing with a small amount of data so
     # there shouldn't be performance issues =)
     for series_name, series_articles in series.items():
-        # This is not DRY but very simple to understand
-        forced_order_articles = [
-            art_tup for art_tup in series_articles if art_tup[0] is not None
-        ]
+        ordered_articles = _sort_articles(series_articles)
 
-        date_order_articles = [
-            art_tup for art_tup in series_articles if art_tup[0] is None
-        ]
-
-        forced_order_articles.sort(key=itemgetter(0))
-        date_order_articles.sort(key=itemgetter(1))
-
-        all_articles = forced_order_articles + date_order_articles
-        ordered_articles = [art_tup[2] for art_tup in all_articles]
-        enumerated_articles = enumerate(ordered_articles)
-
-        for index, article in enumerated_articles:
-            article.series = dict()
-            article.series["name"] = series_name
-            article.series["index"] = index + 1
-            article.series["all"] = ordered_articles
-            article.series["all_previous"] = ordered_articles[0:index]
-            article.series["all_next"] = ordered_articles[index + 1 :]
-
-            if index > 0:
-                article.series["previous"] = ordered_articles[index - 1]
-            else:
-                article.series["previous"] = None
+        for index, article in enumerate(ordered_articles):
+            article.series = {
+                "name": series_name,
+                "index": index + 1,
+                "all": ordered_articles,
+                "all_previous": ordered_articles[0:index],
+                "all_next": ordered_articles[index + 1 :],
+                "previous": ordered_articles[index - 1] if index > 0 else None,
+            }
 
             try:
                 article.series["next"] = ordered_articles[index + 1]
